4b02341f25da2cc6f24d80f6a89b2c272d0a8501
mariams58 <mariams58@yahoo.com>
2022-10-17 20:48:21 -0700

    Alx low-level C

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+/* mmore headers go here */
+
+/**
+  *main - Entry point to te program
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		print("%d is zero\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the project on the C lanuage programming expressions, statements and functions.
