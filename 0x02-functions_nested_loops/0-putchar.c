e74d35e7d0e31923b30d21b0ae8c7da76203e223
mariams58 <mariams58@yahoo.com>
2022-10-18 19:21:27 -0700

    putchar basics

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main- Entry point to the program
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the readme for the alx projects on functions, loops and more
