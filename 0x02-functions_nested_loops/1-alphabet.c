241337a198571cd19e52744291b40ad6eb12a7b9
mariams58 <mariams58@yahoo.com>
2022-10-18 21:23:44 -0700

    alpha1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
-#include < stdio.h >
-#include < main.h >
+#include <stdio.h>
+#include "main.h"
 /**
   *main - Entry point to the program
   *
@@ -16,7 +16,9 @@ int main(void)
 
 void print_alphabet(void)
 {
-	for (i = "a"; i <= "z"; i++)
+	char i;
+
+	for (i = 97; i <= 122; i++)
 	{
 		putchar(i);
 	}
