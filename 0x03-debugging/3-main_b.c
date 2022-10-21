3d98951e5d97ef03ce0b2a1929754d73aa211f1d
mariams58 <mariams58@yahoo.com>
2022-10-20 19:44:17 -0700

    print days

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - takes a darte andoutput days left in a yesr
+  * leap year is takr into account);
+  *Return: 0
+  */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 2;
+	day = 29;
+	year = 20000;
+
+	printf("Date: %02d/%02d04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_remaining_days - takes a date and prits out how manny days are
+  *left in te year
+  *@month:month of the date
+  * @year: year given
+  * @day: day given
+  * Return: void
+  */
+
+void print_remaining_days(int month, int day, int  year)
+{
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	{
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
+
+		printf("Days of the year: %d\n", days);
+		printf("Remaining days: %d\n", 366 - days);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%2d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+
+			printf("Remaining days: %d\n", 365 - days);
+		}
+	}
+}
