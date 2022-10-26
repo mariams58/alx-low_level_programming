ea78f6a56adafee78d9e60e35697a1ef63e9f679
mariams58 <mariams58@yahoo.com>
2022-10-25 21:41:29 -0700

    reverse

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * puts - this prints the reverse of a given string
+  * _puts - this prints the reverse of a given string
   * @str:the given string
   *
   * Return: nothing
