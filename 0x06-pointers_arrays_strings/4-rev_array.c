63194a7fe9ba5a12cc9499a0f2f48a9c910c665a
mariams58 <mariams58@yahoo.com>
2022-10-28 01:09:38 -0700

    reverse arrays

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,18 +9,20 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int rev[n];
+	int rev;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		rev[(n - 1) - i] = *a[i];
+		rev = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = rev;
 	}
 	for (i = 0; i <  n; i++)
 	{
-		*a[i] = rev[i];
-		if (i != 0)
+		if (i != 0 && i < n)
 		{
-			_putchar(", ")
+			_putchar(44);
+			_putchar(32);
 		}
 		_putchar(a[i]);
 	}
