44adfa87c191ac1db8888ac463415c2cc16c751b
mariams58 <mariams58@yahoo.com>
2022-10-27 20:09:12 -0700

    string comparism function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+  * _strcmp - compare stwo strings
+  * @s1: string 1
+  * @s2: string 2
+  *
+  * Return: a number
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+	int x;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0, s2[j] != '\0'; j++)
+	{
+	}
+	if (i == j)
+	{
+		x = i - j;
+	}
+	else if (i > j)
+	{
+		x = i - j;
+	}
+	else if (i < j)
+	{
+		x = i - j;
+	}
+	return (x)
+}
