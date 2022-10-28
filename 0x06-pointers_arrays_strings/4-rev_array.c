6df7939601683e162b1d1a8b059e290d9d0934cc
mariams58 <mariams58@yahoo.com>
2022-10-27 22:43:01 -0700

    rverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
   * reverse_array - prints array in reverse
-  * @ a: pointer to array
+  * @a: pointer to array
   * @n: number
   *
   * Return: 0
   */
 void reverse_array(int *a, int n)
 {
-	int *x;
-	int i;
+	int rev[n];
 
-	for(i = a[n - 1]; i < n &&  i == a[0]; i--)
+	for (i = 0; i < n; i++)
 	{
-		putchar(a[i]);
-	_putchar(10);
+		rev[n - 1 - i] = a[i];
+	}
+	for (i = 0; i <  n; i++)
+	{
+		a[i] = rev[i];
+	}
 	return (0);
 }
