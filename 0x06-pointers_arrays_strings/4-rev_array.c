a3b44ee38126bc54fbc79076b383e490ac97a7c2
mariams58 <mariams58@yahoo.com>
2022-10-27 23:00:00 -0700

    arr rev

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		rev[n - 1 - i] = a[i];
+		rev[n - 1 - i] = *a[i];
 	}
 	for (i = 0; i <  n; i++)
 	{
-		a[i] = rev[i];
+		*a[i] = rev[i];
 	}
 }
