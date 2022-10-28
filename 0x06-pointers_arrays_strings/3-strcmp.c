9c57c06da4ef5a5e20a5850d743f909f78ef14fd
mariams58 <mariams58@yahoo.com>
2022-10-27 22:10:28 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,10 @@ int _strcmp(char *s1, char *s2)
 	int j;
 	char x;
 
-	for (i = 0; s1[i] != 0; i++)
+	for (i = 0; s1[i] ==0; i++)
 	{
 	}
-	for (j = 0; s2[j] != 0; j++)
+	for (j = 0; s2[j] == 0; j++)
 	{
 	}
 	if (i == j)
