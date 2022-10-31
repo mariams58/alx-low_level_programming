8f899a785477730ec0dc9f217a1e03b910cbf86b
mariams58 <mariams58@yahoo.com>
2022-10-31 08:05:26 -0700

    size

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,11 +12,7 @@ void print_number(int n)
 		_putchar('-');
 		n *= -1;
 	}
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	if (n / 10)
+	if (n >= 10)
 	{
 		_putchar(n / 10);
 	}
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -41,7 +41,7 @@ void print_buffer(char *b, int size)
 {
 	int i;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	for (i = 0; i <= size  / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
 
