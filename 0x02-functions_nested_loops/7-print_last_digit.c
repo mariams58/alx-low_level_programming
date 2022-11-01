1c900226c0430005bdb6fc99cb9a604903d8cee1
mariams58 <mariams58@yahoo.com>
2022-11-01 05:04:36 -0700

    jack 24

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * priint_last_digit - prints last digit of a given number
+  * @n: given number
+  *
+  * Return: value of last digit
+  */
+int print_last_digit(int n)
+{
+	int num;
+
+	if (n < 0)
+	{
+		num = (n % 10) * -1;
+		_putchar(ld + '0');
+	}
+	else
+	{
+		num = n % 10;
+		_putchar(num + '0');
+	}
+	return (num);
+}
