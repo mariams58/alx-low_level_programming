234af91fbe743bb6928a3b5dacdf147390535523
mariams58 <mariams58@yahoo.com>
2022-11-08 09:24:02 -0800

    fixes

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,7 +22,7 @@ char *_strdup(char *str)
 		if (str == NULL)
 			return (NULL);
 
-		dest = malloc(sizeof(*dest) * (i + 1));
+		dest = malloc(sizeof(*dest) * i);
 		if (dest != NULL)
 		{
 			while (i > 0)
