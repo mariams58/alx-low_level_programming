68c15d12928d81bbb0b2e6aa8082083364f8a756
mariams58 <mariams58@yahoo.com>
2022-11-08 19:24:05 -0800

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,12 @@ char *_strdup(char *str)
 	char *dest;
 	unsigned int i, j;
 
-	for (i = 0; str[i] == '\0'; i++)
-	{
-		;
-	}
-
 	while (1)
 	{
+		for (i = 0; i != '\0'; i++)
+		{
+			;
+		}
 		dest = (char *)malloc(sizeof(*dest) * (i + 1));
 		if (str == NULL)
 			return (NULL);
