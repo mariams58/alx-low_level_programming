ead300152c8abcc765c9e74a2b6800f31026e950
mariams58 <mariams58@yahoo.com>
2022-11-13 06:01:57 -0800

    argc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,6 +9,28 @@
   */
 char *argstostr(int ac, char **av)
 {
-	char *str, *a;
-	int i, j, k, len = 0;
+	char *str;
+	int i = 1, j, k, len = 0;
+
+	while (1)
+	{
+		while (i < ac)
+		{
+			for (j = 0; j != '\0'; j++)
+				;
+			len += j;
+			i++;
+		}
+		str = malloc(sizeof(char) * len + 1);
+		if (ac == 0 || av == NULL)
+			return (NULL);
+		for (k = 0; k <= len; k++)
+		{
+			str[k] = *av[k];
+			if (*av[k] == 32)
+				str[k] = 10;
+		}
+		return (str);
+	}
+	return (0);
 }
