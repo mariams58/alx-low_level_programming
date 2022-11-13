4b6e3eb4a8fdf4ef898ff660898cd2bd4ff0a2d8
mariams58 <mariams58@yahoo.com>
2022-11-13 07:26:07 -0800

    argv

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,24 +14,24 @@ char *argstostr(int ac, char **av)
 
 	while (1)
 	{
+		if (ac == 0 || av == NULL)
+			return (NULL);
 		while (i < ac)
 		{
-			for (j = 0; av[i][j] != '\0'; j++)
+			for (j = 0; j != '\0'; j++)
 				;
 			len += j;
 			i++;
 		}
 		str = malloc(sizeof(char) * len + 1);
-		if (ac == 0 || av == NULL)
-			return (NULL);
 		if (str == NULL)
 			return (NULL);
 		while (k <= ac)
 		{
-			for (x = 0; av[k][x] != '\0'; x++)
+			for (x = 0; *av[x] != '\0'; x++)
 			{
-				str[x] = av[k][x];
-				if (av[k][x] == 32)
+				str[x] = *av[x];
+				if (*av[x] == 32)
 					str[x] = 10;
 			}
 			k++;
