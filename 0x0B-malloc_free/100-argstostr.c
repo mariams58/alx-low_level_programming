2961b55ad0f4f4a9f0ee003325f773d0da1481d8
mariams58 <mariams58@yahoo.com>
2022-11-13 06:32:31 -0800

    argstr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,7 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i = 1, j, k, len = 0;
+	int i = 1, j, k = 0, len = 0;
 
 	while (1)
 	{
@@ -24,7 +24,7 @@ char *argstostr(int ac, char **av)
 		str = malloc(sizeof(char) * len + 1);
 		if (ac == 0 || av == NULL)
 			return (NULL);
-		for (k = 0; k <= len; k++)
+		while (k <= len)
 		{
 			str[k] = *av[k];
 			if (*av[k] == 32)
