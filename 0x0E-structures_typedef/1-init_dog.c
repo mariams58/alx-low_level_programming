7586a754c67b41b7974aba50d16aafc00a59f058
mariams58 <mariams58@yahoo.com>
2022-11-16 06:52:58 -0800

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#inlude "dog.h"
+/**
+  * init_dog - function
+  * @d: structure dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  *
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
