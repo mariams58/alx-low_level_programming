c896a31555ca987ab3825d042fd3483eb27d9082
mariams58 <mariams58@yahoo.com>
2022-11-15 20:18:03 -0800

    0x0D preprocessing

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+include "main.h"
+/**
+  * main - writes out the name of the current file
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar(__FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is a readme for the alx project on preprocessing
