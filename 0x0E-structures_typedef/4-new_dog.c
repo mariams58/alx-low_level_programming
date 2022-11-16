ca0bafeabc82a24f31671527e989ca17ebb2a136
mariams58 <mariams58@yahoo.com>
2022-11-16 08:54:45 -0800

    newfix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-  *new_dog - stores a new dog
-  * 
+  * new_dog - stores a new dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner detail
+  * Return:  strct of new dog
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -12,8 +15,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = name;
-	ndog->age = age;
+	ndog.name = name;
+	ndog.age = age;
 	ndog->owner = owner;
 	return (ndog);
+	free(ndog);
 }
