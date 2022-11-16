16c8838739a01e34ff8780a781c9ddac6c17b9da
mariams58 <mariams58@yahoo.com>
2022-11-16 06:21:05 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+  * struct dog - A typedef for struct dog
+  * @name: First member
+  * @age: Second member
+  * @owner: third member
+  *
+  * Description: Longer description
+  */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
