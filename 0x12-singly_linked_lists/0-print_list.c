0d4022cd91f79d5a1e90b0f399a5c3f33cc588c3
mariams58 <mariams58@yahoo.com>
2022-11-24 20:45:09 -0800

    new return

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 	size_t num = 0;
 	char *err = "(nil)";
 
-	while (h != NULL)
+	if (h != NULL)
 	{
 		_putchar(91);
 		_putchar(h->len);
@@ -28,7 +28,7 @@ size_t print_list(const list_t *h)
 					err++;
 				}
 		}
-		num++;
+		num += 1;
 	}
 	return (num);
 }
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+  * add_node - returns the address of a new element or null if it failed
+  * @head: pointer to a pointer
+  * @str: pointer to str
+  *
+  * Return: An address to new node
+  */
+
