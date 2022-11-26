898c769502e618f8a6336dda9d08535853ef34e7
mariams58 <mariams58@yahoo.com>
2022-11-26 07:05:52 -0800

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <string.h>
 /**
   * print_all - prints all charcs of strings given
   * @format: pointer to the given list of types of arguments to be passed
@@ -9,50 +7,59 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	char c;
-	int fmt, len = 0, i, j = 0;
-	float f;
-	char *s, *sep = ", ";
 
-	while (format[j] != '\0')
-	{
-		j += 1;
-	}
 	va_start(ap, format);
-	while (format != NULL && format[len])
+	if (format != NULL)
 	{
-		switch (format[len])
+		handle_case(format, ap);
+	}
+	va_end(ap);
+	printf("\n");
+}
+
+/**
+  * handle_case - prints a case output
+  * @format[len]: pointer to array
+  *
+  * Retuen: print format
+  */
+int handle_case(const char * const format, va_list ap)
+{
+
+	va_list aq;
+	int fmt;
+	char *s;
+	size_t i = 0;
+	size_t len = strlen(format);
+
+	va_copy(aq, ap);
+	while (format[i] != '\0')
+	{
+		fmt = 0;
+		switch (format[i])
 		{
 			case 'i':
-				i = va_arg(ap, int);
-				printf("%d", i);
-				fmt = 0;
+				printf("%d", va_arg(aq, int));
 				break;
 			case 'f':
-				f = (float) va_arg(ap, double);
-				printf("%f", f);
-				fmt = 0;
+				printf("%f", va_arg(aq, double));
 				break;
 			case 'c':
-				c = (char) va_arg(ap, int);
-				printf("%c", c);
-				fmt = 0;
+				printf("%c", va_arg(aq, int));
 				break;
 			case 's':
-				s = va_arg(ap, char*);
+				s = va_arg(aq, char*);
 				if (s == NULL)
 					s = "(nil)";
 				printf("%s", s);
-				fmt = 0;
 				break;
 			default:
 				fmt = 1;
 				break;
 		}
-		if (len < j - 1 && fmt == 0)
-			printf("%s", sep);
-		++len;
+		if (i < len - 1 && fmt != 1)
+			printf(", ");
+		i++;
 	}
-	va_end(ap);
-	printf("\n");
+	return (fmt);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,15 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int handle_case(const char * const format, va_list ap);
 
 #endif
