35fb1d136b67c52bda4eba4706069f8d89b5c83f
mariams58 <mariams58@yahoo.com>
2022-11-26 04:11:17 -0800

    free_list recursive

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,19 +6,11 @@
   */
 void free_list(list_t *head)
 {
-	list_t *current;
-
-	current = head->next;
 	if (head != NULL)
 	{
+		free_list(head_next);
 		if (head->str != NULL)
 			free(head->str);
-		while (current != NULL)
-		{
-			if (current->str != NULL)
-				free(current->str);
-			free(current);
-		}
 		free(head);
 	}
 }
