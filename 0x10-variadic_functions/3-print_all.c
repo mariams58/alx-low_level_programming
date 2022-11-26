b6b53508828fa852d7fed3490f258bba98f2388a
mariams58 <mariams58@yahoo.com>
2022-11-26 07:32:53 -0800

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,65 +1,48 @@
 #include "variadic_functions.h"
-#include <string.h>
 /**
   * print_all - prints all charcs of strings given
   * @format: pointer to the given list of types of arguments to be passed
   */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-
-	va_start(ap, format);
-	if (format != NULL)
-	{
-		handle_case(format, ap);
-	}
-	va_end(ap);
-	printf("\n");
-}
-
-/**
-  * handle_case - prints a case output
-  * @format[len]: pointer to array
-  *
-  * Retuen: print format
-  */
-int handle_case(const char * const format, va_list ap)
-{
-
 	va_list aq;
-	int fmt;
+	int i = 0, j = 0, fmt;
 	char *s;
-	size_t i = 0;
-	size_t len = strlen(format);
 
-	va_copy(aq, ap);
-	while (format[i] != '\0')
+	while (format[j] != '\0')
+		j++;
+	if (j > 0)
 	{
-		fmt = 0;
-		switch (format[i])
+		va_start(aq, format);
+		while (format[i] != '\0')
 		{
-			case 'i':
-				printf("%d", va_arg(aq, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(aq, double));
-				break;
-			case 'c':
-				printf("%c", va_arg(aq, int));
-				break;
-			case 's':
-				s = va_arg(aq, char*);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s", s);
-				break;
-			default:
-				fmt = 1;
-				break;
+			fmt = 0;
+			switch (format[i])
+			{
+				case 'i':
+					printf("%d", va_arg(aq, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(aq, double));
+					break;
+				case 'c':
+					printf("%c", va_arg(aq, int));
+					break;
+				case 's':
+					s = va_arg(aq, char*);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s", s);
+					break;
+				default:
+					fmt = 1;
+					break;
+			}
+			if (i < j - 1 && fmt != 1)
+				printf(", ");
+			i++;
 		}
-		if (i < len - 1 && fmt != 1)
-			printf(", ");
-		i++;
+		va_end(aq);
+		printf("\n");
 	}
-	return (fmt);
 }
