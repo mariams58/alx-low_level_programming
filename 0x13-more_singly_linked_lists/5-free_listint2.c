ebd128ae32b28d1d191a1b1b2b9658924f99c2ac
mariams58 <mariams58@yahoo.com>
2022-11-29 20:13:43 -0800

    Task 4- frees the listint_t list and sets head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+  * free_listint2 -  frees the listint_t list and sets head to NULL
+  * @head: pointer to the head of the linked list
+  *
+  */
+void free_listint2(listint_t **head)
+{
+	if (*head != NULL)
+		free_listint2(head->next);
+	free(head);
+	*head = NULL;
+}
