9ccb4007bcadc33b0b913a9643becda60f95c616
mariams58 <mariams58@yahoo.com>
2022-11-29 20:49:32 -0800

    Taskk 6- pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+  * pop_listint - deletes a list fro te top
+  * @head: pointer to pointer
+  *
+  * Return: head's node data
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int x = 0;
+
+	if (*head == NULL)
+		return (0);
+	node = (*head)->next;
+	x = (*head)->n;
+	free(head);
+	*head = next-node;
+	return (x);
+}
