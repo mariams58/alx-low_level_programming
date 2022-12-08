c82743471d9c52a4672cbc7657ffb31e9b668217
mariams58 <mariams58@yahoo.com>
2022-12-08 02:10:16 -0800

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		i += 1;
-		if ((i - index) == 1)
+		if ((i - index == 1) && (head->next != NULL))
 			return (head);
 		if (head->next != NULL)
 			head = head->next;
