c9d9981769450729d780b7990fc2837f572a1f7b
mariams58 <mariams58@yahoo.com>
2022-12-08 06:04:33 -0800

    bug fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,16 +12,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *temp;
 
-	temp = malloc(sizeof(listint_t);
+	temp = malloc(sizeof(listint_t));
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		count += 1;
 		if ((temp != NULL) && count - index == 2)
 		{
-			*temp = temp->next;
+			temp = temp->next;
 			ret = 1;
+			return (ret);
 		}
+
 		*head = temp;
 		free(temp);
 	}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = temp;
 		free(temp);
 	}
-	return (new);
+	return (NULL);
 	free(new->next);
 	free(new);
 }
