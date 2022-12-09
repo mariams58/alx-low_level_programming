54778da1febba2e693c8c143c880cb2d3978cdd0
mariams58 <mariams58@yahoo.com>
2022-12-09 03:24:57 -0800

    task 0: fix 111

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 	if (fd > 0)
 	{
 		num = read(STDIN_FILENO, buf, letters);
