6fd8b074b20c473d08eb9438f61ef7588d7a7b3d
mariams58 <mariams58@yahoo.com>
2022-12-10 00:01:00 -0800

    Task 1.1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] == '\0'; i++)
 			;
-		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
 		num = write(STDIN_FILENO, text_content, i);
