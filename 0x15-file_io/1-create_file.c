3bfdda547600d6a55179f77525a73b89e86ed263
mariams58 <mariams58@yahoo.com>
2022-12-11 08:44:20 -0800

    Task1: fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,6 @@ int create_file(const char *filename, char *text_content)
 	ssize_t num;
 	int fd;
 	size_t i;
-	const void *buffer = &text_content;
 
 	while (filename != NULL)
 	{
@@ -25,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		if (text_content != NULL)
 		{
-			num = write(fd, buffer, i);
+			num = write(fd, text_content, i);
 			if (num == -1)
 				return (-1);
 			else
