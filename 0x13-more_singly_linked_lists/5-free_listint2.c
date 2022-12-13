6b3c910f1c027b0c4828b457a03858f891e606b0
mariams58 <mariams58@yahoo.com>
2022-12-13 04:49:41 -0800

    free poinrr

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,12 @@ void free_listint2(listint_t **head)
 
 	if (*head != NULL)
 		return;
-	while (*head != NULL)
+	new = *head;
+	while (new != NULL)
 	{
 		new = (*head)->next;
 		free(*head);
-		*head = new;
+		*head = NULL;
 		free(new);
 	}
 }
