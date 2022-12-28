5de6a970bcb947c73463990dec9314b48d470968
mariams58 <mariams58@yahoo.com>
2022-12-28 04:39:35 -0800

    comb 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,6 +6,24 @@
   */
 int main(void)
 {
-	int i = 0, j = 0, x = 0, y = 1;
+	int i, j;
 
-	while (y < 
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 1; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(32);
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
