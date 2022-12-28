a07ba419226de830408b71bcf7b9da16d3a8b347
mariams58 <mariams58@yahoo.com>
2022-12-28 07:40:03 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,34 @@
   */
 void times_table(void)
 {
-	int i = 0;
-	int j;
+	int i = 0, j, s;
 
 	while (i < 10)
 	{
 		j = 0;
-
 		while (j < 10)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
-			_putchar(32);
-			_putchar(32);
+			s = i * j;
+			if (s < 10)
+				_putchar(s + '0');
+			else
+			{
+				_putchar((s / 10) + '0');
+				_putchar((s % 10) + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				if ((i * (j + 1)) < 10)
+				{
+					_putchar(32);
+					_putchar(32);
+				}
+				else
+					_putchar(32);
+			}
 			j++;
 		}
-		_putchar(36);
 		_putchar(10);
 		i++;
 	}
