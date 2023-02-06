9eae0029723197b60706f8f9fb3b431e4c93dd59
mariams58 <>
2023-02-05 18:24:59 -0800

    tak 100.3

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,18 +8,17 @@
 
 int _atoi(char *s)
 {
-	int i, j, num = 0, neg = 0;
+	int i = 0, j, num = 0, neg = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		if (s[i] == '-' && (s[i + 1] >= 48 && s[i - 1] > 57))
+		if ((s[i] == '-') && (s[i + 1] < 47 && s[i + 1] > 58))
 			neg += 1;
-		j = i + 1;
-		while ((s[j] >= '0' && s[j] <= '9') && (s[j + 1] > 47 || s[j + 1] < 58))
+		while (s[i] >= '0' && s[i] <= '9')
 		{
-			num = num * 10 + s[j] - '0';
-			j++;
+			num = num * 10 + s[i] - '0';
 		}
+		i++;
 	}
 	if (neg % 2 != 0)
 		num = num * (-1);
