2e88a82d0a489f8e8f1fc7ee60d6ac694a382b3c
mariams58 <>
2023-02-05 17:47:24 -0800

    task atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,26 @@
-#include main.h
+#include "main.h"
+/**
+ * _atoi - returnd integers in string
+ * @s: pointer to str given
+ *
+ * Return: integer in string
+ */
+
+int _atoi(char *s)
+{
+	int i, j, num = 0, neg = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-' && (s[i + 1] >= 48 && s[i - 1] > 57))
+			neg += 1;
+	}
+	j = 0;
+	while ((s[j] >= '0' && s[j] <= '9') && s[j + 1] != 32)
+	{
+		num = num * 10 + s[j] - '0';
+	}
+	if (neg % 2 != 0)
+		num = num * -1;
+	return(num);
+}
