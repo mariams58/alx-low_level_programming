ad37915ad21be6aa862604039fa2e101aa5c1c3d
mariams58 <>
2023-02-05 19:55:10 -0800

    Task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * _atoi - returnd integers in string
  * @s: pointer to str given
@@ -8,19 +9,32 @@
 
 int _atoi(char *s)
 {
-	int i = 0, j, num = 0, neg = 0;
+	int j, i = 0, num = 0, sign = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] == '-') && (s[i + 1] < 47 && s[i + 1] > 58))
-			neg += 1;
-		while (s[i] >= '0' && s[i] <= '9')
-		{
-			num = num * 10 + s[i] - '0';
-		}
-		i++;
+		if (s[i] == '-')
+			sign += 1;
 	}
-	if (neg % 2 != 0)
+	j = 0;
+	while (s[j] != '\0')
+	{
+		if (num > INT_MAX / 10 || (num == INT_MAX / 10 && s[j] - '0' > 7)) 
+		{
+			if (sign % 2 != 0)
+				return INT_MAX;
+			else
+				return INT_MIN;
+		}
+		if (s[j] >= '0' && s[j] <= '9')
+			num = num * 10 + s[j] - '0';
+		if (s[j + 1] < 48 && s[j + 1] > 57)
+			break;
+		else
+			j++;
+	}
+	
+	if (sign % 2 != 0)
 		num = num * (-1);
 	return (num);
 }
