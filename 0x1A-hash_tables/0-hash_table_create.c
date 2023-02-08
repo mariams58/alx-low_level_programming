4a63facc0b8a21da4f2f0a53dab9513b2729114c
mariams58 <>
2023-02-08 04:11:25 -0800

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,14 @@
   */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-	hash_table_t htable = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t* htable = (hash_table_t*) malloc(sizeof(hash_table_t));
 
-	while (htable == NULL)
+	if (htable != NULL)
+	{
+		htable->size = size;
+		htable->array = (hash_node_t**) calloc(htable->size,sizeof(hash_node_t*));
+	}
+	else
 		return (NULL);
-	htable->size = size;
-	htable->array = (hash_node_t **) calloc(sizeof(hash_node_t *));
-	for (i = 0; i < htable->size; i++)
-		htable->item[i] = NULL;
 	return (htable);
 }
