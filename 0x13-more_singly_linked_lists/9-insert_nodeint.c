b510a220ed8a2e30c9c37a8cc1304bfcef7e8662
mariams58 <>
2023-02-11 06:18:00 -0800

    insert at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,8 +17,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		node->n = n;
 		if (*head == NULL)
+		{
 			*head = node;
 			return (*head);
+		}
 		if (idx == 0)
 		{
 			node->next = *head;
