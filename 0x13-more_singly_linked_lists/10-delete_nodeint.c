c1892742d1f5e044ac6ae9deaffd6849fd954e03
mariams58 <>
2023-02-11 15:11:33 -0800

    fix: delnode at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,8 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			if (temp->next == NULL)
 				ret = -1;
-			temp = next->next;
-			free(next);
+			free(temp);
 			ret = 1;
 		}
 		temp = next;
