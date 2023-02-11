60b2fde5ef9a84eff44c1ad86742e3bbaccb7ab1
mariams58 <>
2023-02-11 07:30:31 -0800

    deleteb3

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (ret);
 	if (index == 0)
 	{
-		temp = *head->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 		ret = 1;
