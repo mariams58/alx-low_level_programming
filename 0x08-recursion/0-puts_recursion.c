62840431588882979b316f375fa3d9bf5a43097a
mariams58 <mariams58@yahoo.com>
2023-08-03 04:10:07 -0700

    recursion directory

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _puts_recursion - prints a string folowed by a newline
+  * @s: pointer to string
+  *
+  * Return: nothing
+  */
+void _puts_recursion(char *s);
+{
+	if (*s != '\0')
+	{
+		_putchar(s);
+		_puts_recursion(*(s + 1));
+	}
+}
