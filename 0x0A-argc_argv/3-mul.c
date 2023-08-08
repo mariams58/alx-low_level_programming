409b54cd289ac47dbd333e4a817e8f558d57ae85
mariams58 <mariams58@yahoo.com>
2023-08-08 04:11:29 -0700

    Task 4 - argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * my_prog_mul - prints out arg
+  * @s: pointer to str given
+  *
+  * Return: Always 0
+  */
+int my_prog_mul(char *s)
+{
+	int i;
+
+	i = atoi(s)
+	return (i);
+}
+/**
+  * main - print out name of program
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	int 1 = 0, total = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		while (i < argc)
+		{
+			total *= my_prog_mul(argv[i]);
+		}
+		printf("%d\n", total);
+	}
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -7,4 +7,6 @@ int my_prog(char *s);
 int my_prog1(int s);
 
 int my_prog2(char *s);
+
+int my_prog_mul(char *s);
 #endif
