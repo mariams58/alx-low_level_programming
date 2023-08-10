d1e06002d1409b7b5299c203ba60747b07d916cc
mariams58 <mariams58@yahoo.com>
2023-08-10 10:48:23 -0700

    task 0 - malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+/**
+  * create_array - creates an array oa a given size
+  * @size: size of the array
+  * @c: given character
+  *
+  * Return: an array or Null
+  */
+char *create_array(unsigned int size, char c);
+{
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	while (1)
+	{
+		s = malloc((sizeof(char) * size) + 1);
+		if (s == NULL)
+			return (NULL);
+		s[0] = c;
+		s[1] = "\0";
+		return (s);
+	}
+	return (0)
+}
