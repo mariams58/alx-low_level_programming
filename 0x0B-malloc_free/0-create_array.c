527b7eb001ea5da7d633f2250aa5449e37c3be92
mariams58 <mariams58@yahoo.com>
2023-08-12 05:13:12 -0700

    task  0 - fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	while (1)
 	{
-		s = (char)malloc((sizeof(char) * size) + 1);
+		s = (char *)malloc((sizeof(char) * size) + 1);
 		if (s == NULL)
 			return (NULL);
 		s[0] = c;
 		s[1] = "\0";
 		return (s);
 	}
-	return (0)
+	return (0);
 }
