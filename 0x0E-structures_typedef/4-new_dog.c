e9875682c1e69a08d8645058ac3928ec1d63832a
mariams58 <mariams58@yahoo.com>
2023-08-15 07:11:02 -0700

    Task 4- typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,18 +13,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *my_dog;
 	char *new_name, *new_own;
 
-	my_dog = malloc(sizeof(dog_t));
-	new_name = malloc(sizeof(strlen(name) + 1));
-	new_own = malloc(sizeof(strlen(owner) + 1));
-	if (new_name)
-		strcpy(new_name, name);
-	if (new_own)
-		strcpy(new_own, owner);
-	if (my_dog == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
-
-	my_dog->name = new_name;
-	my_dog->age = age;
-	my_dog->owner = new_own;
-	return (my_dog);
+	while (1)
+	{
+		my_dog = malloc(sizeof(dog_t));
+		new_name = malloc(sizeof(strlen(name) + 1));
+		new_own = malloc(sizeof(strlen(owner) + 1));
+		if (new_name)
+			strcpy(new_name, name);
+		if (new_own)
+			strcpy(new_own, owner);
+		if (my_dog == NULL)
+			return (NULL);
+		my_dog->name = new_name;
+		my_dog->age = age;
+		my_dog->owner = new_own;
+		return (my_dog);
+		free(my_dog->name);
+		free(my_dog->owner);
+		free(my_dog);
+	}
+	return (0);
 }
