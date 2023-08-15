1730487b90addebcbd71891e74a9dc92f1f36003
mariams58 <mariams58@yahoo.com>
2023-08-15 04:55:17 -0700

    macros - task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - perints out name of file
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
