335f4deb34959cf6cd76474a213cb8924e50beba
mariams58 <mariams58@yahoo.com>
2023-08-16 19:24:56 -0700

    Function Pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+  * print_name - prints out name from a given char
+  * @name: pointer to character array of name given
+  * @f: function pointer
+  *
+  * Returrn: Nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+This is the readme file for the Alx project on Function pointers
+in the C programming language
