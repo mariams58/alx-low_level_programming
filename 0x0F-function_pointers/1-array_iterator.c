4787bf8fc1ebbb83ce6d11093d190185b08b9c40
mariams58 <mariams58@yahoo.com>
2023-08-16 19:36:23 -0700

    Task 1 - function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+  * array_iterator - iterate through a given array of numbers
+  * @array: given array
+  * @size: size of the array
+  * @action: function pointer
+  *
+  * Return: Nothing
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size != 0 && array)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
