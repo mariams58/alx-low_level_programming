211e89097738fe89066720667f37be104c67e753
mariams58 <mariams58@yahoo.com>
2023-09-07 01:08:35 -0700

    task 3 - 0x14 bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * set_bit - prints binary equivalent of given num at index
+  * @n: number given
+  * @index: index given
+  *
+  * Return: bit at index or -1
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (*n >= ULONG_MAX)
+		*n = ULONG_MAX;
+	if (*n > 1)
+	{
+		*n = *n | 1 << index;
+		return (1);
+	}
+	return (-1);
+}
