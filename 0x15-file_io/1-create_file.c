fe3cc9b55d253a80465f7d009eda1b289651bdf1
mariams58 <mariams58@yahoo.com>
2023-09-12 21:06:24 -0700

    Task 3 - file IO

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		else
 			return (1);
-		if (text_content)
+		if (text_content != NULL)
 			write(fd, text_content, i);
 		close(fd);
 	}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+  * append_text_to_file - appends text to file
+  * @filename: name of the file given
+  * @text_content: char to string content to append
+  *
+  * Return: Always 1 success, -1 failure
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t i;
+
+	if (filename != NULL)
+	{
+		if (access(filename, F_OK) == -1)
+			return (-1);
+		fd = open(filename, O_APPEND | O_WRONLY);
+		if (fd == -1)
+			return (-1);
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			;
+		}
+		if (access(filename, W_OK) == -1)
+			return (-1);
+		if (text_content != NULL)
+			write(fd, text_content, i);
+		return (1);
+		close(fd);
+	}
+	return (-1);
+}
