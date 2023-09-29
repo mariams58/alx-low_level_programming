18d1130e4372840f424571b2dc540786ad85bc2b
mariams58 <mariams58@yahoo.om>
2023-09-28 21:59:21 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_binary - prints the binary reresentation of a given number
+  * @n: number given to convert to binary
+  */
+void print_binary(unsigned long int n)
+{
+	int x = 1, i = 1;
+
+	if (n > 
+	if (n == 1 || n == 0)
+		_putchar(n + '0');
+	while (x <= n)
+	{
+		x = x * 2;
+		i++;
+	}
+	b = n - x;
+	_putchar(1 + '0');
+	if (b > 0)
+}
