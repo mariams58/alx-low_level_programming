e087a46374e9339b6849aaa685b03526303bf702
mariams58 <mariams58@yahoo.om>
2023-10-07 01:12:09 -0700

    tsk 3 fix - file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,26 +2,22 @@
 #include <stdlib.h>
 #include "main.h"
 
+#define BUFSIZE 1024
+
 /**
   * cl_fd - closes open file descriptor
   * @f1: first file descriptor
-  * @f2: second file descriptor
   *
   * Return: 1 on success
   */
-int cl_fd(int f1, int f2)
+int cl_fd(int f1)
 {
-	int i = 0;
+	int i;
 
-	if (close(f1) == -1)
+	i = close(f1)
+	if (i == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
-		i++;
-	}
-	if (close(f2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
-		i++;
 	}
 	return (i);
 }
@@ -37,27 +33,24 @@ int cl_fd(int f1, int f2)
 int main(int ac, char **av)
 {
 	int res, fd, fd1;
-	char *buf;
+	char buf[BUFSIZE];
 	ssize_t rd, wr;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	buf = malloc(sizeof(char) * 1024);
-	if (buf == NULL)
-		return (1);
-	rd = read(fd, buf, 1024);
+	rd = read(fd, buf, BUFSIZE);
 	if (rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (open(av[2], O_WRONLY | O_TRUNC) == -1)
@@ -72,14 +65,11 @@ int main(int ac, char **av)
 	wr = write(fd1, buf, rd);
 	if (wr == -1 || wr != rd)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-	res = cl_fd(fd, fd1);
-	if (res > 0)
+	res = cl_fd(fd);
+	res1 = cl_fd(fd1);
+	if (res == -1 || res1 != -1)
 		exit(100);
-	close(fd);
-	close(fd1);
-	free(buf);
 	return (0);
 }
