ebe1692b95c5d413b8975d3b1dbabc1a63305f19
mariams58 <mariams58@yahoo.om>
2023-10-10 10:10:51 -0700

    task 3 fix - file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,26 +3,6 @@
 #include "main.h"
 
 #define BUFSIZE 1024
-
-/**
-  * cl_fd - closes open file descriptor
-  * @f1: first file descriptor
-  *
-  * Return: 1 on success
-  */
-int cl_fd(int f1)
-{
-	int i;
-
-	i = close(f1);
-	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
-	}
-	return (i);
-}
-
-
 /**
  * main - check the code
  * @ac: argument count
@@ -32,8 +12,9 @@ int cl_fd(int f1)
  */
 int main(int ac, char **av)
 {
-	int res, fd, fd1, res1;
+	int fd, fd1;
 	char buf[BUFSIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	ssize_t rd, wr;
 
 	if (ac != 3)
@@ -47,29 +28,25 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	rd = read(fd, buf, BUFSIZE);
-	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	if (open(av[2], O_WRONLY | O_TRUNC) == -1)
-		fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+		fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	else
 		fd1 = open(av[2], O_WRONLY | O_TRUNC);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	wr = write(fd1, buf, rd);
-	if (wr == -1 || wr != rd)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	while ((rd = read(fd, buf, BUFSIZE)) > 0)
+	{
+		wr = write(fd1, buf, rd);
+		if (wr == -1 || wr != rd)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
 	}
-	res = cl_fd(fd);
-	res1 = cl_fd(fd1);
-	if (res == -1 || res1 == -1)
-		exit(100);
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
 	return (0);
 }
